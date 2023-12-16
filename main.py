ffa5210d74bbeddf2e785fefdfc1894773a78d39
no-link-to <65858319+no-link-to@users.noreply.github.com>
2023-12-16 16:21:56 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,82 @@
+class SVDWorker:
+    def __init__(self, img):
+        self.image_path = img
+        self.converted_grey = None
+        self.image_matrix = None
+        self.available_values = [100, 90, 80, 70, 60, 50, 40, 10, 5, 1]
+        self.result = []
+
+    @property
+    def image_path(self):
+        return self._image_path
+
+    @image_path.setter
+    def image_path(self, value):
+        self._image_path = value
+
+    @property
+    def converted_grey(self):
+        return self._converted_grey
+
+    @converted_grey.setter
+    def converted_grey(self, value):
+        self._converted_grey = value
+
+    @property
+    def image_matrix(self):
+        return self._image_matrix
+
+    @image_matrix.setter
+    def image_matrix(self, value):
+        self._image_matrix = value
+
+    @property
+    def available_values(self):
+        return self._available_values
+
+    @available_values.setter
+    def available_values(self, value):
+        self._available_values = value
+
+    @property
+    def result(self):
+        return self._result
+
+    @result.setter
+    def result(self, value):
+        self._result = value
+
+    def convert_image_to_grey(self):
+        self.converted_grey = Image.open(self.image_path).convert('L')
+
+    def create_image_matrix(self):
+        if self.converted_grey:
+            self.image_matrix = np.array(self.converted_grey)
+
+    def calculate_results(self):
+        local_result = []
+        for value in self.available_values:
+            # SVD разложение
+            u, s, vh = np.linalg.svd(self.image_matrix, full_matrices=False)
+            # Создание матрицы с меньшим количеством сингулярных чисел
+            local_result.append(u[:, :value] @ np.diag(s[:value]) @ vh[:value, :])
+        self.result = local_result
+
+    def draw_figures(self):
+        for i in range(len(self.result)):
+            plt.figure(figsize=(10, 6))
+            plt.imshow(self.result[i], cmap='gray')
+            plt.title(f'k = {self.available_values[i]}')
+            plt.axis('off')
+            plt.show()
+
+    def run(self):
+        self.convert_image_to_grey()
+        self.create_image_matrix()
+        self.calculate_results()
+        self.draw_figures()
+        
+
+if __name__ == '__main__':
+    svd_worker = SVDWorker("dog_img.jpg")
+    svd_worker.run()
